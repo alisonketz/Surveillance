a8ae1cb6752e0774698c66ff40164accd395c56f
alisonketz <alison.ketz@gmail.com>
2017-12-08 17:07:07 -0600

    Read in data and initial formatting of dates/calculating raw prevalence plot

--- Surveillance_EDA.R
+++ Surveillance_EDA.R
@@ -10,8 +10,43 @@
 ###
 
 setwd("~/Documents/Surveillance")
-library(xlsx)
+
+library(lubridate)
+
+###
+### Read in data. Must be in CSV format, because not enough memory for xlsx 
+### 
+
+d=read.csv("/home/aketz/Documents/Data/WDNR_surveillance/Surveillance_data.csv",header=TRUE)
 
 
+### should redo this when Dan sends updated surveillance dataset
+### as.Date('1/15/2001',format='%m/%d/%Y')
 
-read.xlsx("/home/aketz/Documents/Data/WDNR_surveillance/Surveillance_data.xlsx",sheetName = "Sheet1")
+d$kill_date = as.Date(d$kill_date,c("%Y-%m-%d"))
+head(d$kill_date)
+d$year = year(d$kill_date)
+max(d$year,na.rm=TRUE)
+
+d$positive = as.numeric(d$positive)-1
+table(d$positive)
+
+
+raw.prev=c()
+for(i in 2002:2017){
+    raw.prev=c(raw.prev,sum(d$positive[d$year==i],na.rm=TRUE)/sum(d$year==i,na.rm=TRUE))
+}
+raw.prev
+
+plot(2002:2017,raw.prev)
+
+###
+###
+###
+head(d)
+
+table(d$positive)
+summary(d)
+
+plot(d$kill_date,d$positive)
+
