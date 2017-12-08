0932139ba4c57b39ce760ada7dc238288311d362
alisonketz <alison.ketz@gmail.com>
2017-12-08 16:10:59 -0600

    Initial Commit

---
+++ Surveillance_EDA.R
@@ -0,0 +1,17 @@
+###
+### Surveillance
+###
+### 12/8/2017 
+### EDA
+###
+
+###
+### Preliminaries
+###
+
+setwd("~/Documents/Surveillance")
+library(xlsx)
+
+
+
+read.xlsx("/home/aketz/Documents/Data/WDNR_surveillance/Surveillance_data.xlsx",sheetName = "Sheet1")
